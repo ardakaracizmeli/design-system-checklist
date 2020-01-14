9e91c03bec4585c8f74044348b83d35b861d3b79
Arda Karacizmeli <ardakaracizmeli@gmail.com>
2020-01-14 23:28:56 +0100

    Fixed some CSS issues and broken about page

--- src/routes/AboutRoute/AboutRoute.js
+++ src/routes/AboutRoute/AboutRoute.js
@@ -11,21 +11,10 @@ const AboutText = () => {
       We’ve been working on design systems for a long time. And we’ve spotted the similarities they all share. This website is a collection of best practices to help you build extensive and robust design systems wherever you work.
       <br />
       <br />
-<<<<<<< HEAD
-      Our categories might not look like the system you’ve planned or already have.
-      But this website isn’t meant to be a definitive list of items for every design system.
-      It's a set of guidelines for you to keep track of everything you’ll want to check off to provide
-      a solid user experience.
-      <br />
-      <br />
-      Everything you see on this website is open source. If you’d like to see additional content on this
-      website or if you have feedback, please <a className={s.link} href="https://github.com/ardakaracizmeli/design-system-checklist" target="_blank" rel="noopener noreferrer">contribute</a>.
-=======
       Our categories might not look exactly like the system you’ve planned (or already have). But this isn’t meant to be a definitive list of items for every design system. It's a set of guidelines for you to keep track of everything you’ll want to check off to provide a solid user experience.
       <br />
       <br />
       Everything you see here is open source. If you’d like to see additional content on this website or if you have feedback, please <a className={s.link} href="https://github.com/ardakaracizmeli/design-system-checklist" target="_blank" rel="noopener noreferrer">contribute</a>.
->>>>>>> Copy adjustments
     </p>
   );
 }
