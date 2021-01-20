dda8dee2cc8c91999c7403e4e93ac59cbdbcb651
Yonatan <yonatan@everthere.co>
2021-01-20 15:18:34 +0200

    Changed react-scripts in package.json to higher version and use es6 feature in Hero Component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import s from './Hero.module.css';
+import React from "react";
+import s from "./Hero.module.css";
 
-const Hero = ({ title, subtitle, children }) => {
+const Hero = ({ mainTitle, mainSubtitle, children }) => {
+  let { root, title, subtitle, content } = { s };
   return (
-    <div className={s.root}>
-      <h1 className={s.title}>{title}</h1>
-      <div className={s.subtitle}>{subtitle}</div>
-      { children && <div className={s.content}>{ children }</div> }
+    <div className={root}>
+      <h1 className={title}>{mainTitle}</h1>
+      <div className={subtitle}>{mainSubtitle}</div>
+      {children && <div className={content}>{children}</div>}
     </div>
   );
 };
