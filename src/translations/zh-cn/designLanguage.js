b7ddadc99f15a2592bae90c003ec8d3a64fb85fb
Arlene Xu <53870940+Adansonia-7@users.noreply.github.com>
2023-04-12 04:04:35 +0000

    “design language" to zh-cn

--- src/translations/zh-cn/designLanguage.js
+++ src/translations/zh-cn/designLanguage.js
@@ -1,91 +1,91 @@
 export default {
-  title: "Design language",
+  title: "设计语言",
   description:
-    "Like any language, a design language is a methodical way of communicating with your audience through your approach to product design. It’s the cornerstone of consistent customer experiences.",
+    "正如任何语言一样，设计语言是一种通过你的产品设计方法与你的受众进行沟通的方法。它是客户体验一致性的基石。",
   sections: {
     "dl-brand": {
-      title: "Brand",
+      title: "品牌 (Brand)",
       description:
-        "Brand drives every single decision you make when building new products or features. A good brand is much more than a name and a logo. It’s the values that define your unique identity and what makes you stand out from others.",
+        "品牌驱动着建立新产品或功能时做出的每一项决定。一个好的品牌远不止是一个名字和一个标志。是它的价值定义了独特性，使你脱颖而出。",
       checklist: {
         "dl-brand-vision": {
-          title: "Vision",
+          title: "愿景",
           description:
-            "Why you exist, what your values are and how they’ll help guide the future of your product.",
+            "存在的意义，价值所在，以及它们将如何帮助指导产品的未来。",
         },
         "dl-brand-principles": {
-          title: "Design principles",
+          title: "设计原则",
           description:
-            "The considerations that guide the basis of your practice. They outline how you approach design from a philosophical perspective and help with everyday decisions.",
+            "指导实践基础的考量因素。它们概述了如何从哲学的角度来思考设计，并有助于日常决策。",
         },
         "dl-brand-tone": {
-          title: "Tone of voice",
+          title: "语气",
           description:
-            "A clear tone of voice defines how you speak to your audience at every moment in their journey, helping them get wherever they want to go.",
+            "一个清晰的语气定义了如何在受众的旅程中的每一时刻与他们对话，帮助他们到达他们想去的地方。",
         },
         "dl-brand-terminology": {
-          title: "Terminology",
+          title: "术语",
           description:
-            "Create the standard terms and phrases that need to be kept the same throughout the user experience, speeding up the design process and unifying your voice.",
+            "创建在整个用户体验中保持一致的标准术语和短语，加快设计进程并保持一致。",
         },
         "dl-brand-writing": {
-          title: "Writing guidelines",
+          title: "写作指南",
           description:
-            "Every consistent experience needs watertight writing. Laying down the foundations for your house style early keeps everything in line with consistent grammar, style choices and action-oriented language to help your design.",
+            "每个一致的体验都需要无懈可击的写作。尽早为你的内部风格打下基础。为帮助更好的设计，一切都应与一致的语法、风格以及行动化的语言保持一致",
         },
       },
     },
     "dl-logo": {
-      title: "Logo",
+      title: "标志 (Logo)",
       description:
-        "Most customers form an opinion about a product in seconds. In most cases, your logo will be the first brand asset someone sees. It’s all about making the right first impression. A distinctive logo helps users recognise a product immediately and gives them the essence of your branding.",
+        "大多数客户在几秒钟内就对一个产品形成了看法。在大多数情况下，标志logo将是品牌给人的第一印象。一切都是为了让用户感到惊艳。一个独特的标志可以帮助用户立即识别产品，并让他们了解品牌的本质。",
       checklist: {
         "dl-logo-mono": {
-          title: "Monochrome version",
+          title: "单色版本",
           description:
-            "A monochrome version of your logo that looks good on top of photography or when it’s printed with a poor quality printer.",
+            "标志logo的单色版本，在摄影作品上或用质量差的打印机打印时依然能保持良好的质量。",
         },
         "dl-logo-small": {
-          title: "Small use guidance",
+          title: "小尺寸使用指南",
           description:
-            "Your logo must perform well and be recognisable at all sizes. Tips for using your logo in these cases will minimise the risk of it being misused.",
+            "标志logo必须在各种尺寸下都能良好表现和j具有可识别性。给出在这些情况下使用标志logo的提示，将最大限度地减少它被滥用的风险。",
         },
         "dl-logo-placement": {
-          title: "Placement and clearance guidance",
+          title: "放置和清除指导",
           description:
-            "Your logo must come with clear guidance on how to place it and how to preserve its space since it lives along with other content.",
+            "因为标志logo与其他内容一起存在，所以必须有明确的指导，说明如何放置logo，以及如何保留它的空间。",
         },
         "dl-logo-usage": {
-          title: "Usage guidance",
+          title: "使用指导",
           description:
-            "These are the logo crimes, providing contextual examples of what to (not) do with your logo.",
+            "这些都是标志logo的滥用，提供上下文的例子，说明应该（不）用你的标志做什么。",
         },
         "dl-logo-format": {
-          title: "Different file formats",
+          title: "不同的文件格式",
           description:
-            "Providing a variety of formats for the vector version of your logo will make it easier for others to work and prevent anyone from redrawing it.",
+            "为你的标志logo的矢量版提供各种格式，将方便他人的工作，并预防任何人重新绘制它。",
         },
       },
     },
     "dl-guidelines": {
-      title: "Guidelines",
+      title: "准则 (Guidelines)",
       description:
-        "Providing guidance on how to approach common UX patterns will allow your organisation to establish a consistent approach and a consistent user experience on any platform.",
+        "为如何处理常见的用户体验模式提供指导，将使您的组织在任何平台上建立一致的方法和一致的用户体验。",
       checklist: {
         "dl-guidelines-a11y": {
-          title: "Accessibility",
+          title: "可访问性（无障碍）",
           description:
-            "Guidelines for how you approach accessibility and how you leverage colour, hierarchy and assistive technologies to help your users.",
+            "指导你如何处理可访问性（无障碍）以及如何利用颜色、层级和辅助技术 (assistive technologies) 来帮助你的用户。",
         },
         "dl-guidelines-onboarding": {
-          title: "User onboarding",
+          title: "用户入门",
           description:
-            "How you onboard your users to your product or a new feature and give them a great experience from the start.",
+            "你如何让你的用户进入你的产品或新功能，并从一开始就提供给他们一个良好的体验。",
         },
         "dl-guidelines-microcopy": {
-          title: "Microcopy guidelines",
+          title: "微文案指南 (Microcopy guidelines)",
           description:
-            "The standard way to write for the components in your design system. These take platform conventions and best practices for writing all into consideration.",
+            "为设计系统中的组件编写的标准方式。这些都应考虑到平台惯例和写作的最佳实践方式。",
         },
       },
     },
