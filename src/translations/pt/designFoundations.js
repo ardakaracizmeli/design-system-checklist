018cb6226160977e343300e9f7629050e002eb50
Lais Lobato <laisrlobato@gmail.com>
2023-06-01 23:03:56 -0300

    Added `designFoundations` translations

--- src/translations/pt/designFoundations.js
+++ src/translations/pt/designFoundations.js
@@ -1,177 +1,177 @@
 export default {
-  title: "Foundations",
+  title: "Fundamentos",
   description:
-    "Design assets and tokens that store values for the base layer of your design system, like color and typography. They’re used in components, so changes on this level will resonate throughout the whole system.",
+    "Elementos de design e tokens são elementos que armazenam os valores fundamentais do seu sistema de design, como cores e tipografia. Eles são usados nos componentes, portanto, as alterações nesse nível terão impacto em todo o sistema.",
   sections: {
     "df-color": {
-      title: "Color",
+      title: "Cor",
       description:
-        "Not only an efficient way to showcase your brand but also an efficient way to communicate with your users. Color palettes created with purpose over aesthetics in mind can help you develop intuitive design patterns by adding meaning to your interface.",
+        "Não apenas uma forma eficiente de exibir sua marca, mas também uma maneira eficaz de se comunicar com seus usuários. Paletas de cores criadas com propósito, em vez de apenas estética, podem ajudar você a desenvolver padrões de design intuitivos, adicionando significado à sua interface.",
       checklist: {
         "df-color-a11y": {
-          title: "Accessibility",
+          title: "Acessibilidade",
           description:
-            "Make sure to have accessible pairings between the primary colors in your palette. More importantly, ensure that your background and text colors have at least an AA standard contrast ratio.",
+            "Certifique-se de ter combinações acessíveis entre as cores principais da sua paleta. Além disso, é fundamental garantir que as cores de fundo e de texto tenham um índice de contraste mínimo de acordo com o padrão AA.",
         },
         "df-color-semantic": {
-          title: "Semantic colors",
+          title: "Cores semânticas",
           description:
-            "Besides your brand colors, make sure to have colors defined and made into variables for functions like disabled states, backgrounds, actions, and high-contrast text.",
+            "Além das cores da sua marca, assegure-se de definir e utilizar cores em variáveis para funções como estados desativados, fundos, ações e texto de alto contraste.",
         },
         "df-color-dark-mode": {
-          title: "Dark mode",
+          title: "Tema escuro",
           description:
-            "Preparing a dark mode version of your color palette will allow your design system to adapt to the user's OS color preferences.",
+            "Preparar uma versão de tema escuro da sua paleta de cores permitirá que o seu design system se adapte às preferências de cor do sistema operacional do usuário.",
         },
         "df-color-guidelines": {
-          title: "Guidelines",
+          title: "Diretrizes",
           description:
-            "Provide guidelines on how and when to use the colors in your palette, what to keep in mind when working with them, and how not to use them.",
+            "Disponibilize diretrizes sobre como e quando utilizar as cores da sua paleta, incluindo informações importantes a serem consideradas ao trabalhar com elas, bem como orientações sobre como evitar usos inadequados.",
         },
       },
     },
     "df-layout": {
       title: "Layout",
       description:
-        "A well-thought-out layout goes a long way. Consistent use of a grid and spacing makes it easier for your users to scan the user interface and grasp the content.",
+        "Um layout bem planejado faz toda a diferença. Ao utilizar um grid e espaçamentos de forma consistente, você facilita a escaneabilidade da interface pelo usuário e a compreensão do conteúdo",
       checklist: {
         "df-layout-units": {
-          title: "Units",
+          title: "Unidades",
           description:
-            "Units are the most granular building blocks for layout. Defining a set of values with consistent increments (such as 4, 8, 12, and 16 for a 4-point system) will provide the foundation for designing your grid and spacing values.",
+            "As unidades são os elementos de construção mais granulares do layout. Ao definir um conjunto de valores com incrementos consistentes (como 4, 8, 12 e 16 para um sistema de 4 pontos), você estabelece a base para projetar os valores do grid e de espaçamento.",
         },
         "df-layout-grid": {
           title: "Grid",
           description:
-            "Every layout should sit on a grid that brings order and hierarchy to the interface. Define a grid separately for mobile, tablet, and desktop devices with columns, gutters, and margins so your interface can adapt to any platform quickly.",
+            "Cada layout deve se basear em um grid que traga ordem e hierarquia para a interface. Defina uma grade separadamente para dispositivos móveis, tablets e desktops, com colunas, espaçamentos e margens, para que sua interface possa se adaptar de forma ágil a qualquer plataforma.",
         },
         "df-layout-breakpoints": {
           title: "Breakpoints",
           description:
-            "Predefine the screen sizes and orientations your grid will adapt to.",
+            "Pré-defina os tamanhos de tela e orientações às quais seu grid irá se adaptar.",
         },
         "df-layout-spacing": {
-          title: "Spacing",
+          title: "Espaçamento",
           description:
-            "Horizontal and vertical rhythm plays a significant role in a layout. You should provide straightforward methods for adding space between interface elements independent of your grid.",
+            "O ritmo horizontal e vertical desempenha um papel significativo em um layout. Você deve fornecer métodos diretos para adicionar espaçamento entre os elementos da interface, independentemente do seu grid.",
         },
       },
     },
     "df-typography": {
-      title: "Typography",
+      title: "Tipografia",
       description:
-        "Typography is one of the main ways you surface content in products. A clear hierarchy and contrasting styles in your typography scale will make things easier to read and help with the overall structure of your product. It’s also an opportunity to visualize your brand character and presence.",
+        "A tipografia é uma das principais formas de apresentar conteúdo em produtos. Uma hierarquia clara e estilos contrastantes na escala tipográfica facilitarão a leitura e contribuirão para a estrutura geral do seu produto. É também uma oportunidade para visualizar a personalidade e presença da sua marca.",
       checklist: {
         "df-typography-mobile": {
-          title: "Responsiveness",
+          title: "Responsividade",
           description:
-            "Desktop devices can usually afford to have bigger font sizes compared to mobile devices. Creating a typography scale that adapts to the viewport size will help with a more meaningful hierarchy and layout.",
+            "Em dispositivos desktop, é comum utilizar tamanhos de fonte maiores em comparação com dispositivos móveis. Criar uma escala tipográfica que se adapte ao tamanho da tela ajudará a estabelecer uma hierarquia e um layout mais coerentes e significativos.",
         },
         "df-typography-grid": {
-          title: "Grid relation",
+          title: "Relação do grid",
           description:
-            "Font sizes and leading should match your grid to allow better pairing between text and other UI elements. A good example of this is text paired with icons with bounding boxes.",
+            "Os tamanhos de fonte e o espaçamento devem estar alinhados com o seu grid para permitir uma melhor harmonização entre o texto e os demais elementos da interface. Um bom exemplo disso é o texto combinado com ícones que possuam caixas delimitadoras.",
         },
         "df-typography-readability": {
-          title: "Readability",
+          title: "Legibilidade",
           description:
-            "Optimizing the letter spacing (tracking), line height (leading) and line length for your typography scale will help with the readability of text.",
+            "Otimizar o espaçamento entre letras (tracking), a altura das linhas (leading) e o comprimento das linhas para a sua escala tipográfica ajudará na legibilidade do texto.",
         },
         "df-typography-performance": {
-          title: "Performance",
+          title: "Desempenho",
           description:
-            "Custom fonts need to be downloaded before they can be displayed, especially on the web. Make sure that you have sensible fallbacks and fast loading time for your typography assets. Using system fonts solves this performance problem.",
+            "Fontes personalizadas precisam ser baixadas antes de serem exibidas, especialmente na web. Certifique-se de ter substituições adequadas e um tempo de carregamento rápido para seus recursos tipográficos. Utilizar fontes do sistema resolve esse problema de desempenho.",
         },
         "df-typography-guidelines": {
-          title: "Guidelines",
+          title: "Diretrizes",
           description:
-            "Provide guidelines on how and when to use the pairings in your typography scale, what to keep in mind when working with them, and how not to use them.",
+            "Disponibilize diretrizes sobre como e quando utilizar as combinações da sua escala tipográfica, incluindo informações importantes a serem consideradas ao trabalhar com elas, e orientações sobre como evitar usos inadequados.",
         },
       },
     },
     "df-elevation": {
-      title: "Elevation",
+      title: "Elevação",
       description:
-        "Elevation controls the relative distance between two surfaces along the z-axis. In light mode, it’s usually highlighted by the shadow value applied to a surface, while in dark mode, it’s communicated using the background color value.",
+        "A elevação controla a distância relativa entre duas superfícies ao longo do eixo z. No modo claro, geralmente é destacada pelo valor de sombra aplicado a uma superfície, enquanto no modo escuro, é comunicada usando o valor da cor de fundo.",
       checklist: {
         "df-elevation-shadows": {
-          title: "Shadows",
+          title: "Sombras",
           description:
-            "Provide multiple shadow values based on the supported elevation levels. Most of the time, you will need 3 to 4 elevation levels in your product.",
+            "Forneça múltiplos valores de sombra com base nos níveis de elevação suportados. Na maioria das vezes, você precisará de 3 a 4 níveis de elevação em seu produto..",
         },
         "df-elevation-background": {
-          title: "Background colors",
+          title: "Cores de fundo",
           description:
-            "Each shadow value should have a linked background color. In light mode, these colors might all resolve to the white color since it’s used together with the shadow. In dark mode, they will be used instead of the shadow to communicate the z-axis distance of a surface.",
+            "Cada valor de sombra deve ter uma cor de fundo associada. No modo claro, essas cores podem ser todas resolvidas para a cor branca, já que são usadas juntamente com a sombra. No modo escuro, elas serão utilizadas em vez da sombra para comunicar a distância do eixo-z de uma superfície.",
         },
         "df-elevation-z": {
           title: "Z-index",
           description:
-            "Define a system of z-index values to control which elements have priority to be rendered on top of the others.",
+            "Defina um sistema de valores de z-index para controlar quais elementos têm prioridade para serem renderizados acima dos outros.",
         },
       },
     },
     "df-motion": {
       title: "Motion",
       description:
-        "Shared motion values provide a more coherent user experience and better alignment with the brand.",
+        "O uso de valores de motion compartilhados proporciona uma experiência do usuário mais coerente e alinhada com a identidade da marca.",
       checklist: {
         "df-motion-easing": {
           title: "Easing",
           description:
-            "Provide standard easing functions used across the system for component transitions. As a start, you can use standard, accelerated and decelerated functions that should cover common component use cases.",
+            "Forneça funções de easing padrão usadas em todo o sistema para as transições dos componentes. Como ponto de partida, você pode usar funções de easing padrão, aceleradas e desaceleradas, que devem abranger os casos de uso comuns dos componentes.",
         },
         "df-motion-duration": {
-          title: "Duration",
+          title: "Duração",
           description:
-            "Define multiple values for your animation duration to keep the component transitions consistent across the product",
+            "Defina múltiplos valores para a duração da animação para manter as transições de componentes consistentes em todo o produto.",
         },
         "df-motion-a11y": {
-          title: "Accessibility",
+          title: "Acessibilidade",
           description:
-            "Pay attention to the user's reduced motion preferences and either make the animations less prominent or remove them altogether.",
+            "Esteja atento às preferências do usuário em relação a movimentos reduzidos e adapte as animações para serem menos chamativas ou remova-as completamente, se necessário.",
         },
       },
     },
     "df-iconography": {
-      title: "Iconography",
+      title: "Iconografia",
       description:
-        "Icons are symbols that represent functionality or content. They’re especially recognizable and helpful in user interfaces since their meaning can be understood at a glance. Though they can be used just for decoration, their full potential can be realized when they’re used meaningfully and consistently.",
+        "Ícones são símbolos que representam funcionalidades ou conteúdos. Eles são especialmente reconhecíveis e úteis em interfaces de usuário, pois seu significado pode ser compreendido rapidamente. Embora possam ser usados apenas para decoração, seu potencial completo pode ser percebido quando são utilizados de forma significativa e consistente.",
       checklist: {
         "df-iconography-a11y": {
-          title: "Accessibility",
+          title: "Acessibilidade",
           description:
-            "For icons that convey a meaning or serve a function, offer a default accessible name that expresses that same meaning or function. Screen readers and other assistive technologies may use this name to orient the user about the interface. For purely decorative icons, a name is not required. If your design system exports front-end code, ensure that the icon name is included, for example, using an aria-label.",
+            "Para ícones que transmitem um significado ou têm uma função específica, ofereça um nome acessível padrão que expresse esse mesmo significado ou função. Leitores de tela e outras tecnologias assistivas podem usar esse nome para orientar o usuário sobre a interface. Para ícones puramente decorativos, um nome não é necessário. Se o seu sistema de design exporta código front-end, certifique-se de incluir o nome do ícone, por exemplo, usando o atributo aria-label.",
         },
         "df-iconography-style": {
-          title: "Style",
+          title: "Estilo",
           description:
-            "Make sure that your icon family makes visual sense as a whole. Picking an outlined or filled style and sticking with it will lead to better visual consistency and predictability.",
+            "Certifique-se de que a sua família de ícones tenha uma coesão visual. Escolher um estilo contornado ou preenchido e mantê-lo em todo o produto resultará em uma melhor consistência visual e previsibilidade.",
         },
         "df-iconography-naming": {
-          title: "Naming",
+          title: "Nomenclatura",
           description:
-            "Name your icons based on their communicative purpose rather than how they look. For instance, a triangular media player plays button icon should be named 'play,' not 'triangle.' You can still add related keywords to improve discoverability.",
+            "Atribua nomes aos seus ícones com base em seu propósito comunicativo, em vez de sua aparência. Por exemplo, um ícone de botão de reprodução em formato triangular em um player de mídia deve ser nomeado como 'play' e não como 'triângulo'. Você ainda pode adicionar palavras-chave relacionadas para melhorar a facilidade de descoberta.",
         },
         "df-iconography-grid": {
-          title: "Relation with grid",
+          title: "Relação com o grid",
           description:
-            "Draw your icons in a bounding box that plays well with your grid. This makes for a better pairing with other UI elements. A good example would be icons with bounding boxes paired with text.",
+            "Crie seus ícones dentro de uma caixa delimitadora que esteja alinhada com seu grid. Isso proporcionará uma melhor harmonia com os outros elementos da interface. Um bom exemplo disso é o uso de ícones com caixas delimitadoras combinados com texto.",
         },
         "df-iconography-keywords": {
-          title: "Keywords",
+          title: "Palavras-chave",
           description:
-            "Adding keywords will improve the discoverability of each icon and provide a better user experience for anyone using your system.",
+            "A adição de palavras-chave melhorará a capacidade de descoberta de cada ícone e proporcionará uma melhor experiência do usuário para quem estiver usando seu sistema.",
         },
         "df-iconography-reserved": {
-          title: "Reserved icons",
+          title: "Ícones reservados",
           description:
-            "Reserving icons representing common actions will prevent their use in any other context. System icons for navigation or adding and deleting are good examples. This leads to a more intuitive user experience.",
+            "A reserva de ícones que representam ações comuns evita o uso deles em qualquer outro contexto. Ícones do sistema de navegação, adição e exclusão são bons exemplos. Isso resulta em uma experiência do usuário mais intuitiva.",
         },
         "df-iconography-guidelines": {
-          title: "Guidelines",
+          title: "Diretrizes",
           description:
-            "Provide guidelines on how and when to use icons, what to keep in mind when working with them, and how not to use them.",
+            "Forneça diretrizes sobre como e quando usar ícones, o que ter em mente ao trabalhar com eles e como não utilizá-los.",
         },
       },
     },
